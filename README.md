22c8eeef834f414dce519dff833c6d3d1e0bab9f
sudharani-webonise <sudharani.chegaraddi@weboniselab.com>
2017-11-28 19:26:02 +0530

    react initial set up

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # react-redux
+
+npm init
+
+npm install --save-dev 
+webpack 
+webpack-dev-server 
+react 
+html-webpack-plugin 
+react-redux
+babel-core 
+babel-preset-react
+babel-preset-react-es2015
+babel-loader
+react-dom
+
+create webpack.config.js .gitignore .bablelrc 
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var BUILD = path.resolve(__dirname, 'build');
+var APP = path.resolve(__dirname, 'app');
+var JS = path.resolve(APP, 'javascript');
+
+module.exports = {
+    entry: `${JS}/index.jsx`,
+
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loaders: ['babel-loader']
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loaders: ['babel-loader']
+            }
+        ]
+    },
+
+    output: {
+        filename: 'javascript/[name]-[hash].js',
+        path: BUILD,
+        publicPath: '/'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(JS, 'index.html'),
+            inject: 'body',
+            filename: `${BUILD}/index.html`
+        })
+    ]
+
+}
